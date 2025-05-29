c74c7690ac0b290a59d452b4d2189439109dc76f
abhijeetadhav24 <abhijeetadhav24@gmail.com>
2025-05-29 08:15:03 +0530

    " Second commit " date 29-05"

---
+++ src/test_2805/While_Loop.java
@@ -0,0 +1,17 @@
+package test_2805;
+
+public class While_Loop {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int a=5;
+		while ( a<=10) {
+			
+			System.out.println(" test  : "+a);
+			a++;
+		}
+	}
+
+}
